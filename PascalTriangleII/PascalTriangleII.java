22b37ea8a26c44d451e40339bef229e106891d0b
connieyh <yhc379@nyu.edu>
2015-04-15 19:58:06 -0400

    Reverse Thinking

---
+++ PascalTriangleII/PascalTriangleII.java
@@ -0,0 +1,13 @@
+public class PascalTriangleII {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> row = new ArrayList<>();
+        row.add(1);
+        for(int i=0; i<rowIndex; i++) {
+            row.add(1);
+            for(int j=row.size()-2; j>=1; j--) {
+                row.set(j, row.get(j)+row.get(j-1));
+            }
+        }
+        return row;
+    }
+}
